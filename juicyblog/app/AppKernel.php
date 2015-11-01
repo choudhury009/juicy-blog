06632562b81192df2284585847c3b5f5fdfb14d7
Jannatul Choudhury <jchoudhury18@gmail.com>
2015-11-01 01:57:40 +0000

    setting up fos user bundle and database connection

--- juicyblog/app/AppKernel.php
+++ juicyblog/app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Blogger\BlogBundle\BloggerBlogBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Blogger\UserBundle\BloggerUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
